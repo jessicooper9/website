cada8292af66708200c78f0133e877fd0a6fed8f
jessicooper9 <jessi.cooper@richmond.edu>
2024-03-24 16:58:29 -0400

    commit

---
+++ src/Navigation/Navbar.js
@@ -0,0 +1,28 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <Link to="/" className="site-title">
+                Jessica Cooper
+            </Link>
+            <ul>
+                <CustomLink to="/projects">Projects</CustomLink>
+                <CustomLink to="/aboutme">About Me</CustomLink>
+            </ul>
+        </nav>
+    )
+}
+
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true})
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+    )
+}
---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Jessica Cooper</h1>
+      </header>
+      <main>
+        <p>Welcome to my web page!</p>
+        <img src="https://via.placeholder.com/300" alt="Placeholder" />
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
